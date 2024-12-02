bed963a06842d4e58ff12402c5044b3e46dbc2ad
IDKOS1 <alan@igangjin-ui-MacBookPro.local>
2024-12-02 09:21:52 +0900

    chore: orange, waringColor 추가

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -6,3 +6,5 @@ const middleGray = Color(0xFFAAA6B9);
 const lightGray = Color(0xFFEBEBEB);
 const textColor = Color(0xFF4C4B4B);
 const shadowColor = Color(0x1472B614);
+const orange = Color(0xFFFCA34D);
+const waringColor = Color(0XFFFF6C6C);
