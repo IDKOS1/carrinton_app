e8070037f77eecd172b26efcf1f8a1d8836a266d
IDKOS1 <alan@igangjin-ui-MacBookPro.local>
2024-12-03 11:11:02 +0900

    refactor: 불필요 주석 제거, collect_info_provider.dart 임포트

--- lib/ui/base/base_screen.dart
+++ lib/ui/base/base_screen.dart
@@ -1,7 +1,5 @@
 import 'package:carrinton_app/models/nav_enum_model.dart';
-import 'package:carrinton_app/provider/circle_progress_provider.dart';
-import 'package:carrinton_app/provider/collect_info_provider.dart';
-import 'package:carrinton_app/provider/current_body_provider.dart';
+import 'package:carrinton_app/provider/base/collect_info_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -17,17 +15,6 @@ class BaseScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final collectInfo = ref.watch(collectInfoProvider);
-
-                                                                                                                  // 임의의 값으로 테스트
-    // WidgetsBinding.instance.addPostFrameCallback((_) {
-    //   final read = ref.read(progressNotifierProvider.notifier);
-    //   if (page == Nav.home) {
-    //     read.updateProgress(66.0);
-    //   } else if (page == Nav.collected) {
-    //     read.updateProgress(33.0);
-    //   }
-    // });
-
     return SafeArea(
       child: Scaffold(
         body: Padding(
