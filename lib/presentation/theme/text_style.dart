9e3e8bccfa6c1b04bc73b343fcbe13b63a8ec6ab
IDKOS1 <alan@igangjin-ui-MacBookPro.local>
2024-12-18 15:54:45 +0900

    add: 텍스트 스타일 추가 및 이름 변경

--- lib/presentation/theme/text_style.dart
+++ lib/presentation/theme/text_style.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomStyle {
   static TextStyle appBarTitle({Color color = Colors.black}) {
     return TextStyle(
       color: color,
-      fontSize: 24,
+      fontSize: 24.sp,
       fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle headTitle({Color color = Colors.black}) {
+    return TextStyle(
+      color: color,
+      fontSize: 30.sp,
+      fontWeight: FontWeight.w700
     );
   }
 
   static TextStyle headMedium({Color color = Colors.black}) {
     return TextStyle(
       color: color,
-      fontSize: 16,
+      fontSize: 16.sp,
       fontWeight: FontWeight.w700,
     );
   }
@@ -20,23 +29,39 @@ class CustomStyle {
   static TextStyle boldBody({Color color = Colors.black}) {
     return TextStyle(
       color: color,
-      fontSize: 14,
+      fontSize: 14.sp,
       fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle boldBodyW600({Color color = Colors.black}) {
+    return TextStyle(
+      color: color,
+      fontSize: 14.sp,
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle bodyLarge({Color color = Colors.black}) {
+    return TextStyle(
+      color: color,
+      fontSize: 16.sp,
+      fontWeight: FontWeight.w400
     );
   }
 
   static TextStyle bodyMedium({Color color = Colors.black}) {
     return TextStyle(
       color: color,
-      fontSize: 16,
-      fontWeight: FontWeight.w400
+      fontSize: 14.sp,
+      fontWeight: FontWeight.w400,
     );
   }
 
   static TextStyle bodySmall({Color color = Colors.black}) {
     return TextStyle(
       color: color,
-      fontSize: 14,
+      fontSize: 12.sp,
       fontWeight: FontWeight.w400,
     );
   }
